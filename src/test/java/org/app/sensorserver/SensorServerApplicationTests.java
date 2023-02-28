f471b37d8b9f20acc4d8bdc817c2d345830a8d53
Alex <alexandr.jelia@gmail.com>
2023-02-28 19:08:02 +0300

    init

---
+++ src/test/java/org/app/sensorserver/SensorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.sensorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
